81923b14d65d2b8f89093870717c9b530188f236
112598023 <t112598023@ntut.org.tw>
2023-11-02 19:27:28 +0800

    json_test ok

--- src/json_object.h
+++ src/json_object.h
@@ -1,4 +1,36 @@
 #pragma once
 
+#include "value.h"
+
+#include <map>
 class JsonObject : public Value {
+public:
+    std::string toString() {
+        std::string s = "{\n";
+        // s+= "\"" + sets.begin()->first + "\":";
+        // s+= sets.begin()->second->toString();
+        unsigned n=0;
+        for(const auto& [key, value] : sets){
+            s+= "\"" + key + "\":";
+            s+= value->toString();
+            if( n!=sets.size()-1 ){
+                s+=",\n";
+            }
+            n++;
+        }
+
+        s+= "\n}";
+        return s;
+    }
+
+    void set(std::string key, Value* value){
+        sets.insert( {key, value} );
+    }
+
+    Value* getValue(std::string k){
+        return sets.find(k)->second;
+    }
+
+private:
+    std::map<std::string, Value *> sets;
 };
--- src/string_value.h
+++ src/string_value.h
@@ -1,4 +1,19 @@
 #pragma once
 
+#include <string>
 class StringValue : public Value {
+public:
+    // StringValue(string key) : _key(key) {}
+    StringValue(std::string k){
+        _key = k;
+    }
+
+    std::string toString() {
+        return "\"" + _key + "\"";
+    }
+
+private:
+    std::string _key;
 };
+
+
